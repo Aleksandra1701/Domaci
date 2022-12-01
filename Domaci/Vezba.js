9448c7b59c23b7fa4f79a8d56b642ae9fad11407
Aleksandra Pantic <sandra1701993@gmail.com>
2022-12-01 17:05:30 +0100

    first commit

---
+++ Domaci/Vezba.js
@@ -0,0 +1,17 @@
+let student = {
+    ime: "Bojan", 
+    prezime: "Urosevic", 
+    godine: "22",
+    brojIndexa: "20201410",
+   /* telefon: "381604767967",*/
+    godinaUpisa: "2020", 
+}
+let godinaUpisa = 2021; 
+console.log(student);
+console.log(student.ime, student.prezime, student.brojIndexa);
+
+    if (godinaUpisa == 2020) {
+    console.log('Moze da polaze diplomski') 
+    } else {
+        console.log('Ne moze da polaze diplomski');
+    }
